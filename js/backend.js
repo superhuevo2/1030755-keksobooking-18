c066104af9c93319696bceab5c302a0806116173
Anton <933375@gmail.com>
2019-11-07 14:01:48 +0300

    исправил модуль backend

--- js/backend.js
+++ js/backend.js
@@ -48,44 +48,12 @@
   function load(onLoad, onError) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
-
     makeRequest('load', xhr, null, onLoad, onError)
-
-    // xhr.addEventListener('load', function () {
-    //   try {
-    //     onLoad(xhr.response);
-    //   } catch (err) {
-    //     onError();
-    //   }
-    // });
-    // xhr.addEventListener('error', function () {
-    //   onError();
-    // });
-
-    // xhr.open('GET', URL_LOAD);
-    // xhr.send();
   }
 
   function send(data, onLoad, onError) {
     var xhr = new XMLHttpRequest();
-
     makeRequest('send', xhr, data, onLoad, onError);
-
-
-
-    // xhr.addEventListener('load', function () {
-    //   if (xhr.status === SUCCESS_STATUS) {
-    //     onLoad();
-    //   } else {
-    //     onError();
-    //   }
-    // });
-    // xhr.addEventListener('error', function () {
-    //   onError();
-    // });
-
-    // xhr.open('POST', URL_SEND);
-    // xhr.send(data);
   }
 
   window.backend = {
